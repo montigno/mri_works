a8cc6c18266a3730d4b86cd217f7bc02f62b7e81
montigno <olivier.montigon@univ-grenoble-alpes.fr>
2020-02-21 15:15:34 +0100

    modification of some modules

--- NodeEditor/modules/ImageJ/ImageJ_exec.py
+++ NodeEditor/modules/ImageJ/ImageJ_exec.py
@@ -2,13 +2,10 @@
 
 class ImageJ_execution():
     def __init__(self,command=''):
-        from NodeEditor.python.configStandalone import ConfigModuls
         import os
         import subprocess
         from subprocess import Popen
-        pathImageJ=os.path.normpath(ConfigModuls().getPathConfig('ImageJ'))
-        pathPlugIns=os.path.join(os.path.dirname(pathImageJ),'plugins')
-        subprocess.Popen(['java','-jar',pathImageJ,'-ijpath',pathPlugIns,'-eval',command],shell=False)
+        subprocess.Popen(['ImageJ','-eval',command],shell=False)
         
 ##########################################################################################
 
--- NodeEditor/modules/ImageJ/ImageJ_open.py
+++ NodeEditor/modules/ImageJ/ImageJ_open.py
@@ -24,18 +24,15 @@ class ImageJ_load_multiImages():
 class openImageJ():
 
     def __init__(self, file='path'):
-        from NodeEditor.python.configStandalone import ConfigModuls
         import subprocess
         from subprocess import Popen
         import os
         if file == 'path':
             file = ''
-        pathImageJ = os.path.normpath(ConfigModuls().getPathConfig('ImageJ'))
-        pathPlugIns = os.path.join(os.path.dirname(pathImageJ), 'plugins')
         script_macro = "run(\"Appearance...\", \"interpolate auto menu=15 gui=1 16-bit=Automatic\");"\
                        "run(\"Brightness/Contrast...\");"\
                        "run(\"Enhance Contrast\", \"saturated=0.35\");"
-        subprocess.Popen(['java', '-jar', pathImageJ, file, '-ijpath', pathPlugIns,'-eval',script_macro], shell=False)
+        subprocess.Popen(['ImageJ', file, '-eval',script_macro], shell=False)
         tmp = os.path.basename(file)
         self.currentImg = ('.').join(tmp.split('.')[:-1])
                 
@@ -47,12 +44,9 @@ class openImageJ():
 class openImagej_multiFiles():
 
     def __init__(self, file=['path']):
-        from NodeEditor.python.configStandalone import ConfigModuls
         import subprocess
         from subprocess import Popen
         import os
-        pathImageJ = os.path.normpath(ConfigModuls().getPathConfig('ImageJ'))
-        pathPlugIns = os.path.join(os.path.dirname(pathImageJ), 'plugins')
         list_files = '|||'.join(file)
         script_macro = "run(\"Appearance...\", \"interpolate auto menu=15 gui=1 16-bit=Automatic\");"\
                        "run(\"Brightness/Contrast...\");"
@@ -62,35 +56,29 @@ class openImagej_multiFiles():
                         "open(list[i]);"\
                         "run(\"Enhance Contrast\", \"saturated=0.35\");"\
                         "};"
-        proc = subprocess.Popen(['java', '-jar', pathImageJ, '-ijpath', pathPlugIns, '-eval', script_macro1+script_macro])
+        proc = subprocess.Popen(['ImageJ', '-eval', script_macro1+script_macro])
         
 ##########################################################################################
 
 class ImageJ_macro():
 
     def __init__(self, file_macro='path'):
-        from NodeEditor.python.configStandalone import ConfigModuls
         import subprocess
         from subprocess import Popen
         import os
-        pathImageJ = os.path.normpath(ConfigModuls().getPathConfig('ImageJ'))
-        pathPlugIns = os.path.join(os.path.dirname(pathImageJ), 'plugins')
         option = '-macro'
-        subprocess.Popen(['java', '-jar', pathImageJ, '-ijpath', pathPlugIns, option, file_macro])
+        subprocess.Popen(['ImageJ', option, file_macro])
         
 ##########################################################################################
 
 class ImageJ_macrofile():
 
     def __init__(self, pathImage='path', filemacro='path'):
-        from NodeEditor.python.configStandalone import ConfigModuls
         import subprocess
         from subprocess import Popen
         import os
-        pathImageJ = os.path.normpath(ConfigModuls().getPathConfig('ImageJ'))
-        pathPlugIns = os.path.join(os.path.dirname(pathImageJ), 'plugins')
         option = '-macro'
 #         subprocess.call(['java','-jar',pathImageJ,pathImage,option,filemacro], shell=False)
-        subprocess.Popen(['java', '-jar', pathImageJ, '-ijpath', pathPlugIns, pathImage, option, filemacro])
+        subprocess.Popen(['ImageJ', pathImage, option, filemacro])
         
 ##########################################################################################
