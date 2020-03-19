3c3475ea340fd0b783b366bc0ad3376580a44e77
montigno <olivier.montigon@univ-grenoble-alpes.fr>
2020-03-19 18:15:39 +0100

    add bricks and example for Nifti

--- mri_works/NodeEditor/modules/File_IO/Save_image_nii_gz.py
+++ mri_works/NodeEditor/modules/File_IO/Save_image_nii_gz.py
@@ -17,3 +17,25 @@ class Save_NiiGz:
 
     def pathFile(self: 'path'):
         return self.fileSaved
+    
+##############################################################################
+
+class Save_NiiGz_header:
+    def __init__(self, image=[[0.0]], filepath='path', new_header='', **options):
+        import nibabel as nib
+        import numpy as np
+        from tkinter import filedialog
+
+        if filepath is 'path':
+            self.fileSaved = filedialog.asksaveasfile(**options).name
+        else:
+            if 'nii.gz' in filepath:
+                self.fileSaved = filepath
+            else:
+                self.fileSaved = filepath + '.nii.gz'
+        print("file saved", self.fileSaved)
+        img = nib.Nifti1Image(image, None,header=new_header )
+        nib.save(img, self.fileSaved)
+
+    def pathFile(self: 'path'):
+        return self.fileSaved
