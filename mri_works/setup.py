fe6049ad019cc3058f8f3d269ee8e31e1d3968f0
montigno <olivier.montigon@univ-grenoble-alpes.fr>
2020-08-13 13:20:08 +0200

    software and HTML docs update

--- mri_works/setup.py
+++ mri_works/setup.py
@@ -8,6 +8,7 @@
 
 '''
 Created on 11 feb. 2020
+Modified on 13 aug. 2020
 @author: omonti
 '''
 
@@ -19,46 +20,18 @@ import sys
 def install(command):
     p=subprocess.Popen(command,shell=True)
     p.wait()
-    
-def getCurrentVersion(inf):
-    from packaging import version
-    version = inf[inf.index('Version:')+8:]
-    version=version[:version.index('\n')]
-    return version.strip()
+
     
 if __name__ == '__main__':
     install('pip install --upgrade pip')
-    install('pip3 install packaging')
     install('pip3 install --ignore-installed PyYAML')
     from Config import Config
     pack = Config().getPathLibraries()
     for keypk,valpk in pack.items():
-        version_min=''
-        version_min = valpk[valpk.index('(')+1:valpk.index(')')]
-        valpk=valpk.replace('('+version_min+')', '')
         try:
-            lso = ("pip3 show", keypk)
-            command = " ".join(lso)
-            p = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
-            p.wait()
-            txt, error = p.communicate()
-            inf = txt.decode("utf-8")
-            if inf:
-                if version:
-                    currentVersion = getCurrentVersion(inf)
-                    if version.parse(currentVersion) < version.parse(version_min):
-                        answ = input('\033[93m' +'the version of '+keypk+" installed is old ("+currentVersion+"), update it to the version " + version_min +" or more ? (yes/no) :  ")
-                        if answ == 'yes':
-                            print('\033[93m' +'update '+keypk+' : ')
-                            install(valpk)
-                        else:
-                            print('\033[93m' + keypk+' update canceled ')
-                    else:
-                        print('\033[0m' +keypk,' ....... already installed')
-                else:
-                    print('\033[0m' +keypk,' ....... already installed')
-            else:
-                print('\033[93m' +keypk,' is missing and will be installed ....')
-                install(valpk)
-        except:
-            pass
+            print('\033[93m'+'checking ',keypk,end=' : ')
+            print('\033[0m')
+            install(valpk)
+            print('\033[0m')
+        except Exception as e:
+            print('error : ', e)
