44604e76deebe902bf06bff61c39cd07ce13b4af
montigno <olivier.montigon@univ-grenoble-alpes.fr>
2020-08-25 11:30:10 +0200

    update software

--- mri_works/NodeEditor/modules/File_IO/Deleting_files_folders.py
+++ mri_works/NodeEditor/modules/File_IO/Deleting_files_folders.py
@@ -8,4 +8,24 @@ class deleting_folder:
 class deleting_file:
     def __init__(self, input_file='path'):
         import os
-        os.remove(input_file)
+        try:
+            os.remove(input_file)
+        except OSError as e:
+            print("Error : %s : %s" % (input_file, e.strerror))
+            
+##############################################################################
+
+class deleting_files_model:
+    def __init__(self, input_dir='path', model='*.txt'):
+        import os
+        import glob
+
+        files = glob.glob(os.path.join(input_dir,model))
+
+        for f in files:
+            try:
+                os.remove(f)
+            except OSError as e:
+                print('Error : %s : %s' %(f, e.strerror))
+
+##############################################################################
