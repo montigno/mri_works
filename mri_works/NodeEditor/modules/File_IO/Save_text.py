ace785031a949e642898b0fbba6ee6d9f7837a13
montigno <olivier.montigon@univ-grenoble-alpes.fr>
2020-09-25 15:17:09 +0200

    some blocks updated

--- mri_works/NodeEditor/modules/File_IO/Save_text.py
+++ mri_works/NodeEditor/modules/File_IO/Save_text.py
@@ -1,5 +1,5 @@
 class save_file_text:
-    def __init__(self, text_in='', file_name='text.txt'):
+    def __init__(self, text_in='', file_name='path'):
         text_file = open(file_name, "w")
         text_file.write(text_in)
         text_file.close()
