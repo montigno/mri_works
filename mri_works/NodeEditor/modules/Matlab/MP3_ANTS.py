3ceac58c094f5f1564055ac75be42d5829eb7065
montigno <olivier.montigon@univ-grenoble-alpes.fr>
2020-11-13 15:36:26 +0100

    MP3 ANTs module update

--- mri_works/NodeEditor/modules/Matlab/MP3_ANTS.py
+++ mri_works/NodeEditor/modules/Matlab/MP3_ANTS.py
@@ -2,7 +2,6 @@ class MP3_Atlas_ANTS():
     def __init__(self,
                  mat_eng='',
                  Nifti_in='path',
-                 Json_in='path',
                  Ref_in='path',
                  file_out1='path',
                  file_out2='path',
@@ -10,12 +9,12 @@ class MP3_Atlas_ANTS():
         import matlab.engine
         files_in, files_out = {}, {}
         options['flag_test'] = 0
+        options['Table_in'] = []
         files_in['In1'] = [Nifti_in]
-        files_in['In2'] = [Json_in]
-        files_in['In3'] = [Ref_in]
+        files_in['In2'] = [Ref_in]
         files_out['In1'] = [file_out1]
         files_out['In2'] = [file_out2]
-        mat_eng.Module_Atlas_ANTS(files_in, files_out, options)
+        mat_eng.Module_Atlas_ANTs(files_in, files_out, options)
         self.mat_eng = mat_eng
         self.map = file_out
 
