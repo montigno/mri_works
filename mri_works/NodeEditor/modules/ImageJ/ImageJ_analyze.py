b62497d435a4f1f076db5f258e8169e259a232f8
montigno <olivier.montigon@univ-grenoble-alpes.fr>
2020-11-23 16:37:40 +0100

    add ImageJ Atlas module

--- mri_works/NodeEditor/modules/ImageJ/ImageJ_analyze.py
+++ mri_works/NodeEditor/modules/ImageJ/ImageJ_analyze.py
@@ -6,3 +6,23 @@ class Plot_profil():
 
     def cmd_post(self: 'str'):
         return self.cmd
+    
+##############################################################################
+
+class Atlas_reg():
+    def __init__(self, file_in='path', atlas_in='path', label_txt='path'):
+        import subprocess
+        from subprocess import Popen
+        import os
+        scriptfile = 'open("' + file_in + '");run(\"Enhance Contrast\", \"saturated=0.35\");\n'\
+                     'open("' + atlas_in + '");run(\"Enhance Contrast\", \"saturated=0.35\");\n'
+        script = 'var lines=split(File.openAsString("' + label_txt + '"), "\\n");\n ij.run("Synchronize Windows"); \n'
+        filemacro = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'macros', 'Atlas.txt')
+        scriptmacro = open(filemacro, 'r').read()
+        script += scriptmacro + '\n'
+
+        
+        file_tmp = open("/tmp/tmp.txt", "w")
+        file_tmp.write(script)
+        script = 'run("Install...", "install=/tmp/tmp.txt");'
+        subprocess.Popen(['ImageJ', '-eval', scriptfile, '-eval', script], shell=False)
