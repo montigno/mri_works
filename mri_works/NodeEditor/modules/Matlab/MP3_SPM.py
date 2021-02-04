01f9392f17c37ad9b4bc678dca16059dc763f4da
montigno <olivier.montigon@univ-grenoble-alpes.fr>
2021-02-04 10:32:52 +0100

    examples and blocks added

--- mri_works/NodeEditor/modules/Matlab/MP3_SPM.py
+++ mri_works/NodeEditor/modules/Matlab/MP3_SPM.py
@@ -21,23 +21,25 @@ class MP3_SPM_reorient():
 
 ##############################################################################
 
-class MP_SPM_Coreg_Est_Res():
+class MP3_SPM_Coreg_Est_Res():
     def __init__(self,
                  mat_eng='',
                  file_ref='path',
                  file_source='path',
-                 file_other='path',
+                 files_other=['path'],
                  file_out='path',
+                 files_out_other=['path'],
                  **options):
         import matlab.engine
         files_in, files_out = {}, {}
         options['flag_test'] = 0
         files_in['In1'] = [file_ref]
         files_in['In2'] = [file_source]
-        if file_other != 'path':
-            files_in['In3'] = [file_other]
         files_out['In1'] = [file_out]
         files_out['In2'] = [file_out]
+        if files_other[0] != 'path':
+            files_in['In3'] = files_other
+            files_out['In3'] = files_out_other
         mat_eng.Module_Coreg_Est_Res(files_in, files_out, options)
         self.mat_eng = mat_eng
         self.map = file_out
