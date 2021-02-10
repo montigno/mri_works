36b5ab321cae4071536ee729e71106d80af5ef37
montigno <olivier.montigon@univ-grenoble-alpes.fr>
2021-02-10 14:02:05 +0100

    software update

--- mri_works/NodeEditor/modules/Dcm2nii/Dcm2niix.py
+++ mri_works/NodeEditor/modules/Dcm2nii/Dcm2niix.py
@@ -1,6 +1,6 @@
 class dcm2niix():
     def __init__(self, sourcedir='path', **options):
-        from subprocess import PIPE, run        
+        from subprocess import run        
         import os
         if 'o' in options.keys():
             listRep1 = os.listdir(options['o'])
@@ -13,16 +13,12 @@ class dcm2niix():
         command = ['dcm2niix']
         command.extend(list_options)
         command.append(sourcedir)
-#         result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
         result = run(command, shell=False, check=True)
-        print(result.returncode, result.stdout, result.stderr)
         if 'o' in options.keys():
             listRep2 = os.listdir(options['o'])
         else:
             listRep2 = os.listdir(sourcedir)
         self.listfiles = [os.path.join(options['o'], item) for item in listRep2 if item not in listRep1]
-        
+
     def list_files(self:'list_path'):
         return self.listfiles
-            
-    
