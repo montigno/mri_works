0371b41a2b916385da4a8b502c4c46826017a786
montigno <olivier.montigon@univ-grenoble-alpes.fr>
2021-03-18 08:50:02 +0100

    AntsMotionCorr correction

--- mri_works/NodeEditor/modules/ANTs/fMRI.py
+++ mri_works/NodeEditor/modules/ANTs/fMRI.py
@@ -1,10 +1,19 @@
 class antsMotionCorr:
-    def __init__(self, fixed_image='path', moved_image='path', **options):
+    def __init__(self, output=[''], **options):
         from subprocess import run
         list_options = []
+        list_options.append('--output')
+        list_options.append(output)
         for op in options:
-            list_options.append('-'+op)
+            list_options.append('--' + op)
             list_options.append(options[op])
         command = ['antsMotionCorr']
         command.extend(list_options)            
         result = run(command, shell=False, check=True)
+        self.res = output
+        
+    def outputWarpedImage(self:'path'):
+        return self.res[1]
+    
+    def outputAverageImage(self:'path'):
+        return self.res[2]
